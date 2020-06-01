969f79afff08c49a362320318231610dfc3c7d64
Allen Chen <chen412@gmail.com>
2020-06-01 14:59:15 +0800

    PartOne Finish

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'database_cleaner'
@@ -83,6 +84,7 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
